93e8290d2fae097351326b6cf985701f9f8b5f8c
Zeeshan Haider <zhadier39@gmail.com>
2022-04-04 23:32:16 +0500

    add person.rb that contains person class, it's props and methods

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    @parent_permission || of_age?
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
