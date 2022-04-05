7b931a21f81babe480284a59e44f1756051365f3
Zeeshan Haider <zhadier39@gmail.com>
2022-04-05 22:48:18 +0500

    make person inherit from Nameable so it becomes concrete component

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
@@ -7,10 +9,15 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super
   end
 
   def can_use_services?
     @parent_permission || of_age?
+  end
+
+  def correct_name
+    @name
   end
 
   private
