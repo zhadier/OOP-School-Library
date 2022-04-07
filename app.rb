43fc851803f0f5c8c2b62203fbf61e94c6cd6f55
Zeeshan Haider <zhadier39@gmail.com>
2022-04-08 00:02:10 +0500

    add all required methods using app in entry point main

---
+++ app.rb
@@ -0,0 +1,171 @@
+#!/usr/bin/env ruby
+require_relative './student'
+require_relative './teacher'
+require_relative './rental'
+require_relative './book'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @id = 50
+  end
+
+  def run
+    selection = nil
+    puts 'Welcome to School Library App!'
+    while selection != 7
+      display_options
+      selection = gets.chomp.strip.to_i
+      puts
+      do_task(selection)
+    end
+    puts 'Thank you for using this app!'
+  end
+
+  def display_options
+    puts
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def do_task(selection)
+    case selection
+    when 1
+      list_all_books
+    when 2
+      list_all_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_person_rentals
+    when 7
+      nil
+    else
+      puts 'Please select a valid choice from the list'
+    end
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_selected = gets.chomp.strip.to_i
+
+    case person_selected
+    when 1
+      create_student
+      @id += 1
+    when 2
+      create_teacher
+      @id += 1
+    else
+      puts 'Invalid Selection. Returning to main menu'
+      return
+    end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    if age <= 0 
+      @id -= 1
+      return puts "Wrong input for age. Returning to main menu"
+    end
+    print 'Name: '
+    name = gets.chomp.strip.capitalize
+    print 'Has Parent Permission? [Y/N]: '
+    permission = gets.chomp.strip.upcase
+    case permission
+    when 'Y', 'YES'
+      permission = true
+    when 'N', 'NO'
+      permission = false
+    else
+      puts 'Wrong input for permission'
+      @id -= 1
+      return
+    end
+    student = Student.new(@id, age, nil, name, parent_permission: permission)
+    @people << student
+    puts 'Person created successfully'
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    if age <= 0 
+      @id -= 1
+      return puts "Wrong input for age. Returning to main menu"
+    end
+    print 'Name: '
+    name = gets.chomp.strip.capitalize
+    print 'Specialization: '
+    specialization = gets.chomp.strip.capitalize
+    teacher = Teacher.new(@id, age, specialization, name)
+    @people << teacher
+    puts 'Person created successfully'
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp.strip.capitalize
+    print 'Author: '
+    author = gets.chomp.strip.capitalize
+    book = Book.new(title, author)
+    @books << book
+    puts 'Book created successfully'
+  end
+
+  def list_all_people
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+  def list_all_books
+    @books.each_with_index { |book, index| puts %{#{index}) Title: \"#{book.title}\", Author: #{book.author}} }
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    list_all_books
+    book_selected = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not id)'
+    list_all_people
+    person_selected = gets.chomp.to_i
+    puts
+    print 'Date (YYYY/MM/DD) : '
+    date = gets.chomp.strip
+    Rental.new(date, @books[book_selected], @people[person_selected])
+    puts "Rental created successfully"
+  end
+
+  def list_person_rentals
+    print 'ID of person: '
+    id = gets.chomp.to_i
+    puts 'Rentals: '
+    @people.each do |person|
+      next unless person.id == id
+
+      person.rentals.each do |rental|
+        puts %(Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author})
+      end
+    end
+  end
+end
+
+def main
+  app = App.new
+  app.run
+end
+
+main
